b6aabd76f78ef00d14e296b834b1ab2f9d7c09fb
Samekichi <karthus20@icloud.com>
2023-08-26 22:09:31 +0000

    Adapt to categorized commands by folders.

--- index.js
+++ index.js
@@ -22,22 +22,29 @@ const token = process.env["DISCORD_BOT_SECRET"];
 /* Dynamically load commands */
 const fs = require('node:fs');
 const path = require('node:path');
-// Read in all commmand files
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-// Load all valid commands into bot client
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath)l;
-    // Set a new item in the Collection with the key as the command name
-    // & the value as the exported module.
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+
+// Read in all command categories (folders)
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+// Read in all commmand files
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Load all valid commands into bot client
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name
+        // & the value as the exported module.
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
 }
+
 
 
 /*
@@ -68,12 +75,11 @@ client.on(Events.InteractionCreate, interaction => {
     } catch (error) {
         console.error(error);
         if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true});
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
         } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
-    
 })
 
 
