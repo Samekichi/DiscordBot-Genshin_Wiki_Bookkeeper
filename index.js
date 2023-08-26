5327c0d94feb1327929bba771c1d819f71bd3789
Samekichi <karthus20@icloud.com>
2023-08-26 02:58:14 +0000

    Switch to Replit's tutorial routine with discord.js.

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const token = process.env["DISCORD_BOT_SECRET"];
+
+client.on("ready", () => {
+  console.log("I'm in");
+  console.log(client.user.username);
+});
+
+client.on("messageCreate", (msg) => {
+  console.log(`Detected message: \n\t${msg.author.username} : <${msg.author.id}>\n\t"${msg.content}"`)
+  if (msg.author.id != client.user.id) {
+    // msg.channel.send(msg.content.split("").reverse().join(""));
+    msg.channel.send(`先叫声前辈听听吧~`);
+  }
+});
+
+client.login(token);
